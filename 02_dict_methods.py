76c19bf8aeee99fc6a8691cccb6e6a49cdac4b1b
David Medina <dhmedinac@gmail.com>
2025-10-05 18:34:16 -0500

    Initial commit

---
+++ 02_dict_methods.py
@@ -0,0 +1,13 @@
+marks= {"harry":34, "jack":45,"lily":94}
+
+#print(marks.keys()) # check the keys
+
+#print(marks.values()) # check the values
+
+#print(marks.items()) # check the items
+
+
+marks.pop("harry") # delete one item
+
+print(marks)
+
